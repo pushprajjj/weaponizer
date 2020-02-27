6259b2b3fb329a77cc01aaa3d0f414aa47adeed2
pushprajjj <51404724+pushprajjj@users.noreply.github.com>
2020-02-27 16:56:45 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,10 +9,10 @@ bot.on('message', message =>{
     var msg = message.content;
     var prefix = ">" //The text before commands, you can put anything that you prefer
 
-    if(message.author.id != "674529133174194216" && message.channel.id === "682237400709070880"){
+    if(message.author.id != "682545444852989974" && message.channel.id === "682237400709070880"){
         if(msg.startsWith('.', 0)){
             if(msg === prefix + "Wep" && message.channel.id === "682237400709070880"){
-                message.channel.send('Aim4Justice is here buddy!') // Sends a message to the channel, with the content
+                message.channel.send('WeaponizerYT is here buddy!') // Sends a message to the channel, with the content
             }
             else{
                 message.channel.send(sender + " Hey, I am a infant now. I will be adult when my creator @WeaponizerYT will be free for teach me. Currently i am enjoing here by reading your chat ")
